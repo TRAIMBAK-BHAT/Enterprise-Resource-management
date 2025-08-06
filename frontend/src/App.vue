13dd9961590552eea50db2fa44a50a4515a0239c
TRAIMBAK BHAT <75558011+TRAIMBAK-BHAT@users.noreply.github.com>
2025-08-06 08:25:51 +0530

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Asset Manager</h1>
+    <AssetManager />
+  </div>
+</template>
+
+<script>
+import AssetManager from './components/AssetManager.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AssetManager
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 20px;
+}
+</style>
